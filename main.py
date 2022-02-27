000956c79c580c7314f5aed78d7f1eb6d76b9ef6
Daniil Pavlov <hgh2005dan@gmail.com>
2022-02-27 13:01:09 +0300

    Добавляем капитана

---
+++ main.py
@@ -0,0 +1,128 @@
+from flask import Flask, url_for, request, render_template
+from werkzeug.utils import redirect
+
+from data import db_session
+from data.jobs import Jobs
+from data.users import User
+from forms.loginform import LoginForm
+from forms.user import RegisterForm
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    # db_sess = db_session.create_session()
+    # news = db_sess.query(Jobs).filter(Jobs.is_private != True)
+    return render_template("index.html")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.username.data).first()
+        if user and user.check_password(form.password.data):
+            return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route("/success")
+def success():
+    return render_template('success.html')
+
+
+def user_add():
+    user = User()
+    user.surname = "Scott"
+    user.name = "Ridley"
+    user.age = 21
+    user.position = "captain"
+    user.speciality = "research engineer"
+    user.address = "module_1"
+    user.email = "scott_chief@mars.org"
+
+    user2 = User(surname="Brown",
+                 name="Mark",
+                 age="24",
+                 position="helmsman1",
+                 speciality="engineer",
+                 address="module_2",
+                 email="brown_helmsman@email.ru")
+
+    user3 = User(surname="Malone",
+                 name="Jack",
+                 age="25",
+                 position="helmsman2",
+                 speciality="engineer",
+                 address="module_3",
+                 email="malone_helmsman@email.ru")
+
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.add(user2)
+    db_sess.add(user3)
+    db_sess.commit()
+
+
+def user_get():
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).filter(User.id == 1).first()
+    for news in user.news:
+        print(news.created_date)
+
+
+def jobs_add():
+    pass
+    # db_sess = db_session.create_session()
+    #
+    # news = News(title="Первая новость", content="Привет блог!",
+    #             user_id=1, is_private=False)
+    # user = db_sess.query(User).filter(User.email == "email@email.ru").first()
+    # news2 = News(title="Вторая новость", content="Уже вторая запись!",
+    #              user=user, is_private=False)
+    #
+    # news3 = News(title="Вторая новость", content="Уже вторая запись!",
+    #              user_id=2, is_private=False)
+    #
+    # db_sess.add(news)
+    # db_sess.add(news2)
+    # db_sess.add(news3)
+    # db_sess.commit()
+
+
+if __name__ == '__main__':
+    db_session.global_init("db/blogs.db")
+    user_add()
+    # user_get()
+    # jobs_add()
+    app.run(port=8080, host='127.0.0.1')
